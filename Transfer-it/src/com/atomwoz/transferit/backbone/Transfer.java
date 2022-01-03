e29490e8806be8ebc063bb48fc0098ccad518a76
atomwoz <atwozniak67@gmail.com>
2022-01-03 19:28:23 +0100

    added print writer

--- Transfer-it/src/com/atomwoz/transferit/backbone/Transfer.java
+++ Transfer-it/src/com/atomwoz/transferit/backbone/Transfer.java
@@ -1,11 +1,13 @@
 package com.atomwoz.transferit.backbone;
 
+import java.io.PrintWriter;
+
 public class Transfer
 {
 
 	public static void doAsServer()
 	{
-
+		PrintWriter pw = new PrintWriter(System.out);
 	}
 
 	public static void doAsClient()
